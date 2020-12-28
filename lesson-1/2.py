5559c20681b3f2af70fa6ea23693d811c5d2bad5
Julia-Spitsa <julia.spitsa@gmail.com>
2020-12-28 19:39:53 +0300

    Change time format in 2.py

--- lesson-1/2.py
+++ lesson-1/2.py
@@ -1,6 +1,5 @@
 n = int(input('Введите количество секунд: '))
-hour = str(n//3600)
-minute = str((n % 3600)//60)
-sec = str((n % 3600) % 60)
-print(f'Время: {hour}:{minute}:{sec}')
-
+hour = n//3600
+minute = (n % 3600)//60
+sec = (n % 3600) % 60
+print(f'Время: {hour:02}:{minute:02}:{sec:02}')
